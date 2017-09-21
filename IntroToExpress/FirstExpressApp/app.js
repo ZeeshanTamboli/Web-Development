c9b202a072b1128ead0ac5dff385a4a23c55e204
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2017-09-21 11:23:51 +0530

    FirstExpressApp

--- IntroToExpress/FirstExpressApp/app.js
+++ IntroToExpress/FirstExpressApp/app.js
@@ -6,11 +6,16 @@ app.get("/", function(req, res) {
   res.send("Hi There!");
 });
 // "/bye" => "Goodbye!"
+app.get("/bye", function(req, res){
+	res.send("Goodbye!");
+});
 // "/dog" => "MEOW!"
-
-//Tell Express to listen for requests(start server)
-
-app.listen(process.env.PORT, process.env.IP, function(){
-  console.log("Server has started!!");
+app.get("/dog", function(req, res) {
+	console.log("SOMEONE MADE A REQUEST TO /dog");
+	res.send("MEOW!");
 });
 
+//Tell Express to listen for requests (start server)
+app.listen(3001, function(){
+  console.log("Server has started!!");
+});
