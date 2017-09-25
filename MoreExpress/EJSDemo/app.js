08221264806f28aab0356b3152686692889c56bb
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2017-09-25 14:43:09 +0530

    EJS Cnditionals and loops

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -10,6 +10,16 @@ app.get("/fallinlovewith/:thing", function(req, res) {
 	res.render("love.ejs", {thingVar: thing});
 });
 
+app.get("/posts", function(req, res) {
+	var posts = [
+	{ title: "Post 1", author: "Zeeshan"},
+	{ title: "MongoDB or SQL?", author: "Kunal"},
+	{ title: "ReactJS or AngularJS?", author: "Aditya"}
+	];
+	res.render("posts.ejs", {posts: posts});
+});
+
+
 app.listen(3000, function() {
 	console.log("SERVER IS LISTENING!!!");
 });
