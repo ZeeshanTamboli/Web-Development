23da93e5a67dffefe58cf1301b430393d0e6a805
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2017-10-01 12:08:27 +0530

    Search results

--- APIs/tv_search_app/app.js
+++ APIs/tv_search_app/app.js
@@ -3,8 +3,14 @@ var app = express();
 var request = require("request");
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res) {
+	res.render("search");
+});
+
 app.get("/results", function(req, res) {
-	request("http://imdbapi.poromenos.org/js/?name=game+of+thrones", function(error, response, body) {
+	var query = req.query.search;
+	var url = "http://api.tvmaze.com/search/shows?q=" + query;
+	request(url, function(error, response, body) {
 		if(!error && response.statusCode == 200) 
 		{
 			var data = JSON.parse(body);
@@ -14,5 +20,5 @@ app.get("/results", function(req, res) {
 });
 
 app.listen(3000, function(){
-  console.log("Movie App has started!!");
+  console.log("TV Show App has started!!");
 });
