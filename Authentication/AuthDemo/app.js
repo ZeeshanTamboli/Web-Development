974ff21b2c5f2ca10c4fc94a3c95d5837e37574c
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2017-10-19 11:38:46 +0530

    Register Form

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -12,9 +12,25 @@ mongoose.connect("mongodb://localhost:27017/auth_demo_app", {
   useMongoClient: true,
 });
 
-app.set("view engine", "ejs");
+app.use(require("express-session")({
+  secret: "I like icecreams",
+  resave: false,
+  saveUninitialized: false
+}));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+// ===================
+// ROUTES
+// ===================
+
 
 app.get("/", function(req, res) {
   res.render("home");
@@ -23,6 +39,28 @@ app.get("/", function(req, res) {
 app.get("/secret", function(req, res) {
   res.render("secret");
 });
+
+
+//Auth Routes
+
+//show signup form
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+
+//handling user signup
+app.post("/register", function(req, res) {
+  User.register(new User({username: req.body.username}), req.body.password, function(err, user) {
+    if(err) {
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/secret");
+    });
+  });
+});
+
 
 app.listen(3000, function() {
   console.log("SERVER STARTED!");
